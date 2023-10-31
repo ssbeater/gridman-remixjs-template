f941fd3fb9b8ce1d84a29aeea06c68f57151b7ad
Santiago Suarez <santiagosuarez.beater@outlook.com>
2023-10-31 09:47:57 -0500

    First commit - Remix using express

---
+++ server.ts
@@ -0,0 +1,97 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as url from "node:url";
+
+import { createRequestHandler } from "@remix-run/express";
+import { broadcastDevReady, installGlobals } from "@remix-run/node";
+import express from "express";
+import morgan from "morgan";
+import sourceMapSupport from "source-map-support";
+
+import type { ServerBuild } from "@remix-run/node";
+import type { RequestHandler } from "express";
+
+sourceMapSupport.install();
+installGlobals();
+run();
+
+async function run() {
+  const BUILD_PATH = path.resolve("build/index.js");
+  const VERSION_PATH = path.resolve("build/version.txt");
+
+  const initialBuild = await reimportServer();
+  const remixHandler =
+    process.env.NODE_ENV === "development"
+      ? await createDevRequestHandler(initialBuild)
+      : createRequestHandler({
+          build: initialBuild,
+          mode: initialBuild.mode,
+        });
+
+  const app = express();
+  app.disable("x-powered-by");
+  app.use(
+    "/build",
+    express.static("public/build", { immutable: true, maxAge: "1y" })
+  );
+
+  app.use(express.static("public", { maxAge: "1h" }));
+  app.use(morgan("tiny"));
+  app.all("*", remixHandler);
+  
+  const port = process.env.PORT || 3000;
+  
+  app.listen(port, async () => {
+    console.log(`✅ app ready: http://localhost:${port}`);
+  
+    if (process.env.NODE_ENV === "development") {
+      broadcastDevReady(initialBuild);
+    }
+  });
+  
+  async function reimportServer(): Promise<ServerBuild> {
+    // cjs: manually remove the server build from the require cache
+    Object.keys(require.cache).forEach((key) => {
+      if (key.startsWith(BUILD_PATH)) {
+        delete require.cache[key];
+      }
+    });
+
+    const stat = fs.statSync(BUILD_PATH);
+
+    // convert build path to URL for Windows compatibility with dynamic `import`
+    const BUILD_URL = url.pathToFileURL(BUILD_PATH).href;
+
+    // use a timestamp query parameter to bust the import cache
+    return import(BUILD_URL + "?t=" + stat.mtimeMs);
+  }
+
+  async function createDevRequestHandler(
+    initialBuild: ServerBuild,
+  ): Promise<RequestHandler> {
+    let build = initialBuild;
+    async function handleServerUpdate() {
+      // 1. re-import the server build
+      build = await reimportServer();
+      // 2. tell Remix that this app server is now up-to-date and ready
+      broadcastDevReady(build);
+    }
+    const chokidar = await import("chokidar");
+    chokidar
+      .watch(VERSION_PATH, { ignoreInitial: true })
+      .on("add", handleServerUpdate)
+      .on("change", handleServerUpdate);
+
+    // wrap request handler to make sure its recreated with the latest build for every request
+    return async (req, res, next) => {
+      try {
+        return createRequestHandler({
+          build,
+          mode: "development",
+        })(req, res, next);
+      } catch (error) {
+        next(error);
+      }
+    };
+  }
+}
